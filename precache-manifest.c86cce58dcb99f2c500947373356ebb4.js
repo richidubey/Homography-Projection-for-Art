9709d7f4bbbb139afdb71da86b7297db2155782f
Richi Dubey <richidubey@gmail.com>
2024-12-21 13:16:03 -0500

    Updates

---
+++ precache-manifest.c86cce58dcb99f2c500947373356ebb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309a7125ce7a9589240d3436315a3de5",
+    "url": "/Homography-Projection-for-Art/index.html"
+  },
+  {
+    "revision": "2eb507f455d604ccc130",
+    "url": "/Homography-Projection-for-Art/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "281a63437772e8ab8ea1",
+    "url": "/Homography-Projection-for-Art/static/js/2.4b7976d3.chunk.js"
+  },
+  {
+    "revision": "04d28f2dbf3a34249bd810e9c89aff63",
+    "url": "/Homography-Projection-for-Art/static/js/2.4b7976d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0dd1b9d5e374e132a4b",
+    "url": "/Homography-Projection-for-Art/static/js/3.87ad7167.chunk.js"
+  },
+  {
+    "revision": "2eb507f455d604ccc130",
+    "url": "/Homography-Projection-for-Art/static/js/main.ac50defd.chunk.js"
+  },
+  {
+    "revision": "41e7128538bce7e91327",
+    "url": "/Homography-Projection-for-Art/static/js/runtime-main.2b2b83e3.js"
+  }
+]);
